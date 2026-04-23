44d4c2b82562ef6ae1b4c913374d2e8393e2c1cf
Ethan <69767295+ethpac@users.noreply.github.com>
2026-04-22 20:16:31 -0500

    Update get_alchemer_responses.R

--- R/get_alchemer_responses.R
+++ R/get_alchemer_responses.R
@@ -15,7 +15,7 @@ get_alchemer_responses <- function(id, page, resultsperpage) {
   } else if (stringr::str_length(Sys.getenv('ALCHEMER_KEY') > 0)) {
     response <- httr::GET(url = stringr::str_glue("https://api.alchemer.com/v5/survey/{id}/surveyresponse?api_token={Sys.getenv('ALCHEMER_KEY')}&api_token_secret={Sys.getenv('ALCHEMER_SECRET')}&page={page}&resultsperpage={resultsperpage}")) |>
       httr::content("parsed")
-    responses <- as.data.frame(do.call(rbind, response$data))
+    responses <- as.data.frame(do.call(rbind, as.list(response$data)))
   } else {
     print("ERROR: Failed to get Alchemer responses")
   }
